71a6a416e57f9b5b000612bf4a90eaae7e095677
manugowda230 <manugowda230.6@gmail.com>
2024-12-16 23:39:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,13 +4,13 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 # Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("API_ID", "25929889"))
 
 # Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "fd980dbd069e0b45d0dec91f7e616bad")
 
 # Your Owner / Admin Id For Broadcast 
-ADMINS = int(os.environ.get("ADMINS", "6073523936"))
+ADMINS = int(os.environ.get("ADMINS", "5456381819"))
 
 # Your Mongodb Database Url
 DB_URI = os.environ.get("DB_URI", "")
